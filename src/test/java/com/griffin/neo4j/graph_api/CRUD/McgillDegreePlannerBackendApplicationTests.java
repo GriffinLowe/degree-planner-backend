97eafdae442b7ef5776a2bde0bee54c8398c720f
griffinlowe <griffin.lowe@airboss.com>
2025-08-07 16:30:50 -0400

    First commit: setup user authentication

---
+++ src/test/java/com/griffin/neo4j/graph_api/CRUD/McgillDegreePlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griffin.neo4j.graph_api.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McgillDegreePlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
